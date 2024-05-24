97fd3d72d665045d97f788c381f320e890e89279
Laura Jenniches <laura.jenniches@helmholtz-hiri.de>
2024-05-24 13:58:03 +0200

    organizing scripts for Shigella publication figures

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,3 @@
-#' custum colors
-cb_palette_8 <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-cb_palette <- c("#193cbcff", "#1473afff", "#589acfff", "#89c3efff", "#ea594eff", "#e5b039ff", "#ede65aff")
-cb_palette_4 <- c(viridis(4)[1:3], cb_palette[6])
-
-get_viridis_color <- function(x, opt="D", begin=0, end=1, reverse=FALSE) {
-  x <- x * (end - begin) + begin
-  cmap <- viridisLite::viridis.map[viridisLite::viridis.map$opt == opt,]
-  if (reverse) cmap <- cmap[rev(seq_len(nrow(cmap))),]
-  map_rgbs <- grDevices::rgb(cmap$R, cmap$G, cmap$B)
-  ramp <- grDevices::colorRamp(map_rgbs, space="Lab", interpolate="spline")
-  out_rgbs <- ramp(x) / 255
-  grDevices::rgb(out_rgbs[,1], out_rgbs[,2], out_rgbs[,3])
-}
-
 fancy_scientific <- function(l) {
   # turn in to character string in scientific notation
   l <- format(l, scientific = TRUE)
